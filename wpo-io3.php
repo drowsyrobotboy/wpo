cac4462ed8f73fadb6fdd8cb9acf39c5d860bebf
Maruthi Sharma <maruthisharma@live.in>
2014-03-17 13:51:46 +0530

    version1 ready

--- wpo-io3.php
+++ wpo-io3.php
@@ -1,17 +1,12 @@
 <?php
 /*======================================================
 ----------------------------------------------------
-****** - The Image optimization Module - Part I **********
+****** - The Image optimization Module - Part III -- For other browsers **********
 ----------------------------------------------------
-This Module converts JPEG image files to WebP format and optimizes png/gif images.
+This Module converts optimizes jpg/png/gif images.
 -------------------------------
 Changelog
 -------------------------------
-version 1: directory under test contains 119 images and takes roughly 43 seconds to convert. 
-version 2: directory under test contains 119 images and takes roughly 33 seconds to convert. 
-version 3: different approach. png/gif are no more converted to webp. 
-
-Note: THE TIME TAKEN TO CONVERT CAN VARY BASED UPON THE FORMAT OF IMAGE PRESENT IN MAJORITY
 -------------------------------
 Important Notes
 -------------------------------
@@ -19,19 +14,6 @@ set_time_limit used extend time limit to 120s
 all functions have been reduced to single function to reduce execution time
 =========================================================*/
 include('header.php'); 
-
-//the old main function
-function wpo_webp_main_old($dir) {
-	$xml = simplexml_load_file($dir); // load the xml file
-	$wp_list = array(); // create an empty array for SimpleXMLObjects
-	$wp_list = array_merge($wp_list, $xml->xpath("/wpo/file")); // add paths of files to $list array
-	foreach($wp_list as $value) {
-		$path = str_replace("..", "out", $value->path);// path to destination file in "out" folder 
-		unlink($path); //delete unconverted image file in "out" folder
-		$path = str_replace(pathinfo($path, PATHINFO_EXTENSION), "webp", $path); // prepeare destination file with .webp extension
-		exec("cwebp ".$value->path." -o ".$path. " -q 100"); // execute external command cwebp to convert given image to webp
-	}
-}
 
 //the new main function - faster conversion
 function wpo_webp_main($dir) {
@@ -41,16 +23,16 @@ function wpo_webp_main($dir) {
 	foreach($wp_list as $value) {
 		$path = str_replace("..", "out", $value->path);// path to destination file in "out" folder
 		if(((pathinfo($value->path, PATHINFO_EXTENSION)) == "jpg")||((pathinfo($value->path, PATHINFO_EXTENSION)) == "jpeg")) {
-		unlink($path); //delete unconverted jpg image file in "out" folder
-		$path = str_replace(pathinfo($path, PATHINFO_EXTENSION), "webp", $path); // prepeare destination file with .webp extension
-		/*let php - GD function handle jpg conversions */
-		
-			imagewebp(imagecreatefromjpeg($value->path), $path);
+			unlink($path); //delete unconverted jpg image file in "out" folder
+			imagejpeg(imagecreatefromjpeg($value->path), $path, 70);
 		}
-		/*//cwebp tool handles png/gif conversions to preserve transparency and smoothness
-		else { 
-			exec("cwebp ".$value->path." -o ".$path. " -q 100"); // execute external command cwebp to convert given image to webp
-		}*/
+		else if((pathinfo($value->path, PATHINFO_EXTENSION)) == "png") {
+			unlink($path); //delete unconverted png image file in "out" folder
+			$png = imagecreatefrompng($value->path);
+			imagealphablending($png, false);
+			imagesavealpha($png, true);  
+			imagepng($png,$path,9);
+		}
 	}
 }
 
